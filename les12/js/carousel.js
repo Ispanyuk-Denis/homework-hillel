c6c8bb6c88d15bd98eca40302d62b5c31ab57746
Ispanyuk-Denis <ispanukdenis@gmail.com>
2022-06-04 22:11:41 +0300

    ffff

---
+++ les12/js/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    arrows: false,
+    fade: true,
+    cssEase: 'linear'
+    });
+});
---
+++ less15/js/carousel.js
@@ -0,0 +1,9 @@
+$('.carousel').slick({
+    infinite: true,
+    dots: false,
+    arrow: false,
+    autoplay: true,
+    speed: 300,
+    fade: true,
+    cssEase: 'linear'
+});
